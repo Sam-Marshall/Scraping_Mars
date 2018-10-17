407fc34c5853aca27c319fc2515001dca9544c0d
Stacy Ann Marshall <stacy.marshall@northwestern.edu>
2018-10-17 17:16:51 -0500

    deployment

--- scrape_mars.py
+++ scrape_mars.py
@@ -21,7 +21,7 @@ def scrape():
     # In[3]:
 
 
-    executable_path = {'executable_path': '/app/.apt/usr/bin/google-chrome-stable'}
+    executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
     browser = Browser('chrome', **executable_path, headless=False)
 
 
